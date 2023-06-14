39ef6082e47ecdb45c6fe3616a9fdd2972f65dbe
lNikol <lazar.nikolic@itcentar.rs>
2023-06-14 10:24:03 +0200

    FEAT: implemented bugydebugy companion

---
+++ Companions/BugyDebugy.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Companions;
+
+use JetBrains\PhpStorm\NoReturn;
+
+class BugyDebugy
+{
+    protected mixed $variable;
+
+    #[NoReturn] public function bugy(mixed $variable): void
+    {
+        echo $this->title();
+        echo $this->newLine(2);
+
+        ob_start();
+        var_dump($variable);
+
+        $output = ob_get_clean();
+
+        echo $this->styleHighlightedOutput($output);
+
+        echo $this->newLine();
+        echo $this->title();
+
+        die();
+    }
+
+    protected function styleHighlightedOutput(string $output): string
+    {
+        $style = "\"background-color: lightgray; padding: 10px;\"";
+
+        return "<div style=$style><pre>" . highlight_string("<?php\n" . $output, true) . "</pre></div>";
+    }
+
+    protected function traces(): array
+    {
+        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
+
+        return $trace[0];
+    }
+
+    protected function newLine(int $lines = 1): string
+    {
+        $newLine = '<br>';
+
+        foreach (range(1, $lines) as $line) {
+            $newLine .= $newLine;
+        }
+
+        return $newLine;
+    }
+
+    protected function title(string $title = null): string
+    {
+        if (! $title) {
+            $title = '#################### <i>DEBUG</i> ####################';
+        }
+
+        return $deBugTitle = "<strong style=\"background-color: red; padding: 5px; margin-bottom: 5px; border-radius: 5px;\">{$title}</strong>";
+    }
+}
--- Helpers/helpers.php
+++ Helpers/helpers.php
@@ -12,25 +12,8 @@ if (! function_exists('env')) {
 if (! function_exists('deb')) {
     #[NoReturn] function deb(mixed $variable): mixed
     {
-        $deBugTitle = '<strong style="background-color: red; padding: 5px; border-radius: 5px;">#################### <i>DEBUG</i> ####################</</strong>';
+        $bugyDebugy = new \Companions\BugyDebugy();
 
-        echo $deBugTitle;
-        echo '<br>';
-
-        ob_start();
-        var_dump($variable);
-
-        $output = ob_get_clean();
-
-        $style = "\"background-color: gray; padding: 10px;\"";
-
-        $highlightedOutput = "<div style=$style><pre>" . highlight_string("<?php\n" . $output, true) . "</pre></div>";
-
-        echo $highlightedOutput;
-
-        echo '<br>';
-        echo $deBugTitle;
-
-        die();
+        $bugyDebugy->bugy($variable);
     }
 }
